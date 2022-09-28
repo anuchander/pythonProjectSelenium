277d004b9ccf42b7c6932b63797445b6b54a84a3
anuchander <54872530+anuchander@users.noreply.github.com>
2022-09-28 13:26:37 -0700

    adding new changes to shadowRootDemo.py

---
+++ append_string_to_file.py
@@ -0,0 +1,17 @@
+file1 = open("/Users/anu/PycharmProjects/pythonProjectSelenium/myfile.txt", 'w')
+L = ["This is Delhi\n", "This is Paris\n", "This is London\n"]
+file1.writelines(L)
+file1.close()
+
+#append adds at last
+file1 = open("/Users/anu/PycharmProjects/pythonProjectSelenium/myfile.txt", 'a')
+file1.write("This is Singapore\n")
+file1.close()
+
+file1 = open("/Users/anu/PycharmProjects/pythonProjectSelenium/myfile.txt", 'r')
+print(file1.read())
+file1.close()
+
+file1 = open("/Users/anu/PycharmProjects/pythonProjectSelenium/myfile.txt", 'w')
+file1.write("This is tomorrow\n")
+file1.close()
---
+++ closest_pair_from_two_sorted_array_x.py
@@ -0,0 +1,26 @@
+import sys
+
+def printClosest(ar1, ar2, m, n, x):
+    diff = sys.maxsize
+    print(diff)
+    l = 0
+    r = n-1
+    while(l<m and r>=0):
+        if abs(ar1[l]+ar2[r]-x)<diff:
+            res_l=l
+            res_r=r
+            diff=abs(ar1[l]+ar2[r]-x)
+
+        if ar1[l]+ar2[r]>x:
+            r=r-1
+        else:
+            l=l+1
+
+    print("The closest pair is [", ar1[res_l], ",", ar2[res_r], "]")
+
+ar1 = [1, 4, 5, 7, 8, 9]
+ar2 = [10, 20, 30, 40]
+m = len(ar1)
+n = len(ar2)
+x=38
+printClosest(ar1, ar2, m, n, x)
