3b1438cd5a9ccc32b98c6affe4bf45a2b1493820
anuchander <54872530+anuchander@users.noreply.github.com>
2022-10-20 11:59:26 -0700

    add duplicate_element program 10/20/22

---
+++ duplicate_element.py
@@ -0,0 +1,17 @@
+a=['a', 'a','b','c','d', 'b']
+b=[]
+for x in a:
+    if x not in b:
+        b.append(x)
+print(b)
+
+count_char={}
+count_char()
+count
+for x in a:
+    count_char(x, 1)
+
+
+
+    if x not in count_char:
+        count_char.
